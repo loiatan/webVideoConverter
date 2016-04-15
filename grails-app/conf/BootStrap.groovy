de706d2f4503dbb2244a3e7cad03390a2038c0cc
Rodrigo Guimaraes <rodrigo@agileoperations.com.br>
2016-04-15 08:28:42 -0400

    Added Spring Security plugin to restrict access to web application;

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,28 @@
+import com.agileoperations.webvideoconverter.Role
+import com.agileoperations.webvideoconverter.User
+import com.agileoperations.webvideoconverter.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+		createDefaultUsersAndRoles()
     }
+	
+	def createDefaultUsersAndRoles() {
+		def devOpsRole = Role.findByAuthority('ROLE_DEVOPS') ?: new Role(name: 'DevOps Administrator', authority: 'ROLE_DEVOPS').save(failOnError: true)
+
+		def username = "rodrigo@agileoperations.com.br"
+		def devOpsUser = User.findByUsername(username) ?: new User(
+				username: username,
+				password: 'notTheRealPassword',
+				enabled: true).save(failOnError: true)
+
+		if (!devOpsUser.authorities.contains(devOpsRole)) {
+			UserRole.create devOpsUser, devOpsRole
+		}
+		println "CREATED!"
+	}
+	
     def destroy = {
     }
 }
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -115,3 +115,21 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.agileoperations.webvideoconverter.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.agileoperations.webvideoconverter.UserRole'
+grails.plugin.springsecurity.authority.className = 'com.agileoperations.webvideoconverter.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                ['ROLE_DEVOPS'],
+	'/index':           ['ROLE_DEVOPS'],
+	'/index.gsp':       ['ROLE_DEVOPS'],
+	'/assets/**':       ['permitAll'],
+	'/**/js/**':        ['permitAll'],
+	'/**/css/**':       ['permitAll'],
+	'/**/images/**':    ['permitAll'],
+	'/**/favicon.ico':  ['permitAll'],
+	'/dbconsole/**':  ['permitAll']
+]
+
