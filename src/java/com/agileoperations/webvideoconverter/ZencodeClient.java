ceaed928612f92d26545e7d72389cfabf2268a9a
Rodrigo Guimaraes <rodrigo@agileoperations.com.br>
2016-04-17 19:51:26 -0400

    Removed hardcoded dummy POST body request and replaced for a body receiving real parameters from uploaded video vile; Incremented application version;

--- src/java/com/agileoperations/webvideoconverter/ZencodeClient.java
+++ src/java/com/agileoperations/webvideoconverter/ZencodeClient.java
@@ -2,6 +2,8 @@ package com.agileoperations.webvideoconverter;
 
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
@@ -10,18 +12,34 @@ import com.mashape.unirest.request.body.Body;
 
 public class ZencodeClient {
 	
+	// Injecting log4j
+	Logger log = Logger.getLogger(getClass());
+	
 	public ZencodeClient(){
 	}
 	
 	public HttpResponse<JsonNode> encodeToWeb(Map<String, String> uploadedS3FileInfo) throws UnirestException{
 		
 		String uploadedFileLocation = uploadedS3FileInfo.get("uploadedFileLocation");
+		String timestamp = uploadedS3FileInfo.get("timestamp");
+		String multipartFilename = uploadedS3FileInfo.get("multipartFilename");
+		
+		String jsonBody = "{\"input\":" + "\"" + uploadedFileLocation + "\"" 
+				+ ", \"test\":\"true\""
+				+ ", \"outputs\": ["
+				+ "{\"url\":" + "\"s3://agileoperations.com.br/webvideoconverter/output/"+ timestamp + "/" + multipartFilename + "\""
+				+ ", \"label\": \"mp4 low\""
+				+ ", \"size\": \"640x480\""
+				+" }]"
+				+ "}";
+		
+		log.info("POST request body: " + jsonBody);
 		
 		HttpResponse<JsonNode> jsonResponse = Unirest.post("https://app.zencoder.com/api/v2/jobs")
 				.header("accept", "application/json")
 				.header("Content-Type", "application/json")
 				.header("Zencoder-Api-Key", "8c8cc8f67df571aeab9c450ea3fde3c4")
-				.body("{\"input\":" + "\"" + uploadedFileLocation + "\", \"test\":\"true\"}")
+				.body(jsonBody)
 				.asJson();
 		
 		return jsonResponse;
